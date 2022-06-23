25f254cdcb1be1e908272363dff89635327f0c79
Etienne Renoton <etienne.renoton@gmail.com>
2022-06-23 17:23:03 +0200

    Like Hate 1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Colleague } from './models/colleague';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'top-colleagues';
+  Colleague1 = {
+    pseudo: 'Jean',
+    score: 15000,
+    photo: 'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?k=20&m=1179420343&s=612x612&w=0&h=G2UGMVSzAXGAQs3pFZpvWlHNRAzwPIWIVtSOxZHsEuc=',
+
+  };
 }
---
+++ src/app/models/colleague.ts
@@ -0,0 +1,6 @@
+export interface Colleague {
+  pseudo: String;
+  score: number;
+  photo: String;
+
+}
