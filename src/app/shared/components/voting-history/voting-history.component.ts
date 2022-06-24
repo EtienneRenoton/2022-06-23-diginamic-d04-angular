0b33f0d4a3072d73ed3752cf935e9fb3400e1812
Etienne Renoton <etienne.renoton@gmail.com>
2022-06-24 17:13:03 +0200

    Affichage liste de voting

--- src/app/shared/components/voting-history/voting-history.component.ts
+++ src/app/shared/components/voting-history/voting-history.component.ts
@@ -1,8 +1,9 @@
+import { Colleague } from 'src/app/models/colleague';
 import { Vote } from './../../../models/vote';
 
 import { Component, Input, OnInit } from '@angular/core';
 import { LikeHate } from './../../../models/like-hate';
-import { Colleague } from './../../../models/colleague';
+
 
 
 @Component({
@@ -12,16 +13,44 @@ import { Colleague } from './../../../models/colleague';
 })
 export class VotingHistoryComponent implements OnInit {
 
-  @Input() vote!: Vote
+  @Input() voteList: Vote[] = [
+    {
+      colleague:
+      {
+        pseudo: 'Jean',
+        score: 150,
+        photo: 'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?k=20&m=1179420343&s=612x612&w=0&h=G2UGMVSzAXGAQs3pFZpvWlHNRAzwPIWIVtSOxZHsEuc=',
 
-  toString(choix:LikeHate):string{
-    if (choix == LikeHate.LIKE){
-      return "aimé(e)"
-    }
-    else{
-      return "detesté(e)"
-    }
-  }
+      },
+      vote: LikeHate.HATE
+    },
+    {
+      colleague:
+      {
+        pseudo: 'Jean',
+        score: 150,
+        photo: 'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?k=20&m=1179420343&s=612x612&w=0&h=G2UGMVSzAXGAQs3pFZpvWlHNRAzwPIWIVtSOxZHsEuc=',
+
+      },
+
+      vote: LikeHate.LIKE
+    },
+    {
+      colleague:
+
+      {
+        pseudo: 'Jean',
+        score: 150,
+        photo: 'https://media.istockphoto.com/photos/smiling-man-outdoors-in-the-city-picture-id1179420343?k=20&m=1179420343&s=612x612&w=0&h=G2UGMVSzAXGAQs3pFZpvWlHNRAzwPIWIVtSOxZHsEuc=',
+
+      },
+      vote: LikeHate.HATE
+    },
+
+  ];
+
+
+
   constructor() { }
 
   ngOnInit(): void {
